774b648ef477a862ccf475ecbb1e0c2b8914db98
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-08-06 20:40:45 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
+#
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from typing import Union
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from ArchMusic import app
+
+
 def start_pannel(_):
     buttons = [
         [

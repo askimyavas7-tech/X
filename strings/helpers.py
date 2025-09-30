f4c86fac6e5a480ad83eb50a93fb8dd59e3b3bcb
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-09-30 11:55:00 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -74,37 +74,14 @@ HELP_4 = """🥏<u>**💥OYNATMA KOMUTU
 """
 HELP_5= """🥏<u>** ✨Oynatma Listeleri:
 
+📥 /bul /indir [müzik adı] veya [Youtube linki]: Youtube'dan müzik indirir.
+
 ❤️‍🩹 /playlist: Oynatma listenizi gösterir.
 🆑 /listemisil: Listeden müzik siler.
 📃 /oynat: Oynatma listesi ekranı açar
 """
 
-HELP_6= """🥏<u>**MUZİK İNDİRME KOMUTU**</u>
-
-🗨️ /sarkisozu [müzik adı]: Şarkı sözlerini arar.
-📥 /bul /indir [müzik adı] veya [Youtube linki]: Youtube'dan müzik indirir.
-
-"""
-HELP_7=  """🥏<u>**GRUP ENGELLE BLACKLIST CHAT FUNCTION </u>**
-
-/blacklistchat veya /engelle [CHAT_ID] - Music Bot'u kullanarak yapılan tüm sohbetleri kara listeye alın
-/whitelistchat [CHAT_ID] - Kara listeye alınmış herhangi bir sohbeti Music Bot kullanarak beyaz listeye alın
-/blacklistedchat veya /engelliler- Kara listeye alınan tüm sohbetleri kontrol edin.
-/gban [Kullanıcı adı veya kişisel yanıt] - Bir kullanıcıyı botun sunduğu sohbetten yasaklayın ve onun botunuzu kullanmasını engelleyin.
- /ungban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un yasaklı listesinden kaldırın ve onun botunuzu kullanmasına izin verin
-/gbannedusers - G Yasaklı Kullanıcı Listelerini Kontrol Edin
-  """
-HELP_8= """🥏<u>**REKLAM KOMUTLARI BROADCAST FUNCTION </u>**
-/broadcast [Mesaj Gönderme veya Mesaja Cevap Verme] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
-╰☞**-pin** : Bu, mesajınızı sabitleyecektir
-╰☞**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
- ╰☞**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
- ╰☞**-assistant** :Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır .
-  ╰☞**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlayacaktır
- **Example:╰☞** `/broadcast -user -assistant -pin Merhaba Testi
-"""
-
-HELP_9=  """🥏<u>**BOT KOMUTLARİ
+HELP_6=  """🥏<u>**BOT KOMUTLARİ
  /start: Başlatma panelini gösterir.
 ⚙️ /ayarlar: Ayarlar menüsüne ulaşır.
 🆘 /yardim: Yardım menüsüne ulaşır.
@@ -120,6 +97,22 @@ HELP_9=  """🥏<u>**BOT KOMUTLARİ
 /addsudo [Username or Reply to a user] Sudo ekler
 /delsudo [Username or Reply to a user] sudo listesinde cikarir
  🆘 /sudolist: Sudo kullanıcılarına ulaşır.
+ REKLAM KOMUTU
+ /broadcast [Mesaj Gönderme veya Mesaja Cevap Verme] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
+╰☞**-pin** : Bu, mesajınızı sabitleyecektir
+╰☞**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
+ ╰☞**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
+ ╰☞**-assistant** :Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır .
+  ╰☞**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlayacaktır
+      ENGELEME KOMUTU 
+ **Example:╰☞** `/broadcast -user -assistant -pin Merhaba Testi
+/blacklistchat veya /engelle [CHAT_ID] - Music Bot'u kullanarak yapılan tüm sohbetleri kara listeye alın
+/whitelistchat [CHAT_ID] - Kara listeye alınmış herhangi bir sohbeti Music Bot kullanarak beyaz listeye alın
+/blacklistedchat veya /engelliler- Kara listeye alınan tüm sohbetleri kontrol edin.
+/gban [Kullanıcı adı veya kişisel yanıt] - Bir kullanıcıyı botun sunduğu sohbetten yasaklayın ve onun botunuzu kullanmasını engelleyin.
+ /ungban [Kullanıcı adı veya kullanıcıya yanıt] - Bir kullanıcıyı Bot'un yasaklı listesinden kaldırın ve onun botunuzu kullanmasına izin verin
+/gbannedusers - G Yasaklı Kullanıcı Listelerini Kontrol Edin
+  
 """
 
 
